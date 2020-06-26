8be1ef459e0301515a87a1d239f8e5fd21bd69b0
Annie Lang <jianwen.lang@gmail.com>
2020-06-26 13:43:45 -0700

    Create trapping-rain-water.py

---
+++ Week_01/trapping-rain-water.py
@@ -0,0 +1,19 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left, right = 0, len(height)-1
+        maxL, maxR = 0, 0
+        res = 0
+        while left <= right:
+            if height[left] <= height[right]:
+                if height[left] >= maxL: 
+                    maxL = height[left]
+                else:
+                    res += maxL - height[left]
+                left += 1
+            else:
+                if height[right] >= maxR:
+                    maxR = height[right]
+                else:
+                    res += maxR - height[right]
+                right -= 1
+        return res
