c6c515d9407c82129e4cdbe7d7f54847f7584e51
Annie Lang <jianwen.lang@gmail.com>
2020-06-30 17:34:05 -0700

    Create pre-OrderN-aryTree.py

---
+++ Week_02/pre-OrderN-aryTree.py
@@ -0,0 +1,33 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+# recursion
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return 
+        res = []
+        self.traverseP(root, res)
+        return res
+    def traverseP(self, root, res):
+        if not root:
+            return res
+        res.append(root.val)
+        for x in root.children:
+            self.traverseP(x, res)
+# iteration
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root: 
+            return []
+        s, res = [root], []
+        while s:
+            tmp = s.pop()
+            res.append(tmp.val)
+            if tmp.children:
+                s.extend(tmp.children[::-1])
+        return res
