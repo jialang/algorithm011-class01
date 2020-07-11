47a8800270b72e17716604ee9179774096834824
Annie Lang <jianwen.lang@gmail.com>
2020-07-11 15:19:51 -0700

    Create constructTree-105.py

---
+++ Week_03/constructTree-105.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder, inorder):
+        if not preorder or not inorder:
+            return None
+
+        val = preorder.pop(0)
+        root = TreeNode(val)
+        idx = inorder.index(val)
+        ltree = inorder[:idx]
+        rtree = inorder[idx+1:]
+        root.left  = self.buildTree(preorder, ltree)
+        root.right = self.buildTree(preorder, rtree)
+
+        return root
