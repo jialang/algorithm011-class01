32a4c0f4585f50b29b238eb876c6fd7b5cf8df52
Annie Lang <jianwen.lang@gmail.com>
2020-07-11 14:51:45 -0700

    Create combination-77.py

---
+++ Week_03/combination-77.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        self.findC(1, [], res, n, k)
+        return res
+    def findC(self, level, tmp, res, n, k):
+        if len(tmp) == k:
+            res.append(tmp[:])
+            return
+        for i in range(level, n+1):
+            self.findC(i+1, [i]+tmp, res, n, k)
