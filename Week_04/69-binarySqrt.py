0d2ccb1db21b3d2baef3dda41ed1b31a5f36cf39
Annie Lang <jianwen.lang@gmail.com>
2020-07-18 19:47:35 -0700

    Create 69-binarySqrt.py

---
+++ Week_04/69-binarySqrt.py
@@ -0,0 +1,18 @@
+def sqt(x):
+    if x < 2:
+        return x
+
+    left, right = 2, x // 2
+
+    while left <= right:
+        pivot = left + (right - left) // 2
+        num = pivot * pivot
+        if num > x:
+            right = pivot - 1
+        elif num < x:
+            left = pivot + 1
+        else:
+            return pivot
+
+    return right
+print(sqt(8))
