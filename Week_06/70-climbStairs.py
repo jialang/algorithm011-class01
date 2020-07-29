0ca68f5c3b1f7737416c531c10789eea563064f7
Annie Lang <jianwen.lang@gmail.com>
2020-07-28 18:40:43 -0700

    Update 70-climbStairs.py

--- Week_06/70-climbStairs.py
+++ Week_06/70-climbStairs.py
@@ -6,3 +6,13 @@ class Solution:
         for _ in range(1, n):
             a, b = b, a+b
         return b
+    def climbStairs123(self, n):
+        if n < 1:
+            return 0
+        if n <= 2:
+            return n
+        a, b, c, d = 1, 1, 2, 0
+        for i in range(3, n+1):
+            d = a+b+c
+            a, b, c = b, c, d
+        return d
