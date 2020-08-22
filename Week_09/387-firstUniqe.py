c7d30efe565f3d515c5dd22669b15de680d1201f
Annie Lang <jianwen.lang@gmail.com>
2020-08-22 16:19:42 -0700

    Create 387-firstUniqe.py

---
+++ Week_09/387-firstUniqe.py
@@ -0,0 +1,14 @@
+from collections import OrderedDict
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d = OrderedDict()
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]] = (1, i)
+            else:
+                d[s[i]] = (2, i)
+        for k in d:
+            if d[k][0] == 1:
+                return d[k][1]
+        return -1
