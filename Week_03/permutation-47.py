3b97b67a945590eb773a659c2e6420d6d6c6d0e7
Annie Lang <jianwen.lang@gmail.com>
2020-12-29 20:38:34 -0800

    Update permutation-47.py

--- Week_03/permutation-47.py
+++ Week_03/permutation-47.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 class Solution:
-    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+    def permuteUnique(self, nums):
         res, n = set(), len(nums)
         for x in permutations(nums, n):
             res.add(x)
