3df4d1ffd21a434999f29b70c5871e1603772cdc
Annie Lang <jianwen.lang@gmail.com>
2020-12-29 20:57:23 -0800

    Update 200-newtonPerfectSqrt.py

--- Week_04/200-newtonPerfectSqrt.py
+++ Week_04/200-newtonPerfectSqrt.py
@@ -1,7 +1,7 @@
-    def isPerfectSquare(self, num):
-        if num == 1:
-            return True
-        r = num
-        while r * r > num:
-            r = (r+num/r) // 2
-        return (r*r == num)
+def isPerfectSquare(self, num):
+    if num == 1:
+        return True
+    r = num
+    while r * r > num:
+        r = (r+num/r) // 2
+    return (r*r == num)
