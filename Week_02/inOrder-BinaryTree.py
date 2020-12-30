18be26360af7f2b0ca118c00a5b5d5c641b0150e
Annie Lang <jianwen.lang@gmail.com>
2020-12-29 20:48:38 -0800

    Update inOrder-BinaryTree.py

--- Week_02/inOrder-BinaryTree.py
+++ Week_02/inOrder-BinaryTree.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:            
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
+    def inorderTraversal(self, root):
         s, res = [], []
         tmp = root
         while tmp or s:
