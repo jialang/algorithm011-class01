c9fc7ea2355f0a241afc835848a1360ae008bb3d
Annie Lang <jianwen.lang@gmail.com>
2020-12-29 20:43:02 -0800

    Update coin-change.py

--- Week_06/coin-change.py
+++ Week_06/coin-change.py
@@ -1,5 +1,5 @@
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
+    def coinChange(self, coins, amount):
         a = [float('inf')] * (amount+1)
         a[0] = 0
         for i in range(1, amount+1):
