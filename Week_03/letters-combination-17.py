7d9ccc4ea2ab021b1debc2ba6de06b18fed29007
Annie Lang <jianwen.lang@gmail.com>
2020-12-29 20:36:49 -0800

    Update letters-combination-17.py

--- Week_03/letters-combination-17.py
+++ Week_03/letters-combination-17.py
@@ -1,6 +1,6 @@
 class Solution:
     LETTERS = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, digits):
         if not digits:
             return []
         ans, level, s = [], 0, ''
