9d402d32c7ea1c6dabafc51df844a10616e08562
Annie Lang <jianwen.lang@gmail.com>
2020-12-29 20:45:15 -0800

    Update plusone.py

--- Week_01/plusone.py
+++ Week_01/plusone.py
@@ -1,5 +1,5 @@
 class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+    def plusOne(self, digits):
         for i in reversed(range(len(digits))):
             if digits[i] < 9:
                 digits[i] += 1
